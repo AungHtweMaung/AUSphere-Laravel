9fe8d1171c29a19febc8a07a593bdaef7efd4baa
AungHtweMaung <aunghtwemaung@securelinkmm.com>
2025-08-13 21:45:44 +0630

    Add uni logo in login, register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            {{-- <x-authentication-card-logo /> --}}
+            <img src="{{ asset('src/assets/images/uni_logo1.png') }}" width="130px" height="130px" alt="">
+
         </x-slot>
 
         <x-validation-errors class="mb-4" />
